5110d3d669a4bcddf763a32b6fc3130f1a93da55
JamesHD001 <henry.jameshd@gmail.com>
2026-01-07 16:40:24 +0100

    Add user input for first and last name in Exercise1; enhance grading feedback in Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your is your grade?(type only the grade number i.e 65) ");
+        int userGrade = int.Parse(Console.ReadLine());
+        if (userGrade >= 90)
+        {
+            Console.Write($"Your grade {userGrade}, is A");
+        }
+        else if (userGrade >= 80 && userGrade < 90)
+        {
+            Console.Write($"Your grade {userGrade}, is B");
+        }
+        else if (userGrade >= 70 && userGrade < 80)
+        {
+            Console.Write($"Your grade {userGrade}, is C");
+        }
+        else if (userGrade >= 60 && userGrade < 70)
+        {
+            Console.Write($"Your grade {userGrade}, is D");
+        }
+        else
+        {
+            Console.Write($"Your grade {userGrade}, is F");
+            Console.WriteLine("Try harder next semester! I know you can do it.");
+        }
+        if (userGrade >= 70)
+        {
+            Console.Write("Congratulations! You passed the test/exam");
+            Console.Write("Keep Up The Work!");
+        }
+        else
+        {
+            Console.Write("Unfortunately your grade is too low.");
+            Console.Write("Try harder next time, you can do it!");            
+        }
     }
 }
