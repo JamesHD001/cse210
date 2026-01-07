d48c9ca58f7302e6703eb77600c0ca4cc8886a83
JamesHD001 <henry.jameshd@gmail.com>
2026-01-07 23:30:04 +0100

    Enhance user interaction and feedback in Guess The Magic Number Game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.WriteLine("Welcome To The Guess The Magic Number Game");
+        // Random randomGenerator = new Random();
+        // int number = randomGenerator.Next(1, 11);
+        Console.Write("What is the Magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        int userGuess = 0;
+        do
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+            if (userGuess < magicNumber)
+            {
+                 Console.WriteLine("Higher");
+            }
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        }   while (userGuess != magicNumber);
+
+        Console.WriteLine("You guessed it!");
     }
 }
