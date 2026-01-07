4d66c72febe910f353d0e112723b96fc35e13e67
JamesHD001 <henry.jameshd@gmail.com>
2026-01-07 19:06:27 +0100

    Refactor grade calculation logic and output for clarity in Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -9,31 +9,49 @@ class Program
         Console.Write("What is your grade?(type only the grade number i.e 65) ");
         int userGrade = int.Parse(Console.ReadLine());
         string letterGrade = ("");
+        string modifier = "";
+        int lastDigit = userGrade % 10;
+        
         if (userGrade >= 90)
         {
             letterGrade = ("A");
-            Console.WriteLine($"Your grade {userGrade}, is {letterGrade}");
         }
         else if (userGrade >= 80 && userGrade < 90)
         {
             letterGrade = ("B");
-            Console.WriteLine($"Your grade {userGrade}, is {letterGrade}");
         }
         else if (userGrade >= 70 && userGrade < 80)
         {
             letterGrade = ("C");
-            Console.WriteLine($"Your grade {userGrade}, is {letterGrade}");
         }
         else if (userGrade >= 60 && userGrade < 70)
         {
             letterGrade = ("D");
-            Console.WriteLine($"Your grade {userGrade}, is {letterGrade}");
         }
         else
         {
             letterGrade = ("F");
-            Console.WriteLine($"Your grade {userGrade}, is {letterGrade}");
         }
+
+        if (lastDigit >= 7)
+        {
+            modifier = ("+");
+        }
+        else if (lastDigit < 3)
+        {
+            modifier = ("-");
+        }
+        if (letterGrade == "A" && modifier == "+")
+        {
+            modifier = "";
+        }
+        else if (letterGrade == "F")
+        {
+            modifier = "";
+        }
+        Console.WriteLine($"Your grade {userGrade}, is {letterGrade}{modifier}");
+
+
         if (userGrade >= 70)
         {
             Console.WriteLine("Congratulations! You passed the test/exam");
